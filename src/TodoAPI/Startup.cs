49b7002b69e6a6a85f9678aba0bf0431f52ee37f
Nag <mknagarajan@gmail.com>
2021-02-22 20:31:16 -0500

    HttpsRedirection() commented

--- src/TodoAPI/Startup.cs
+++ src/TodoAPI/Startup.cs
@@ -44,7 +44,7 @@ namespace TodoAPI
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TodoAPI v1"));
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
 
             app.UseRouting();
 
