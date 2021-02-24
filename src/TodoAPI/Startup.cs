8d2b7638dd84e76b62802c3379e07d2d43a447f5
Nag <mknagarajan@gmail.com>
2021-02-24 08:30:09 -0500

    Inmemory and Automapper included.

--- src/TodoAPI/Startup.cs
+++ src/TodoAPI/Startup.cs
@@ -11,6 +11,9 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using TodoApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 namespace TodoAPI
 {
@@ -26,7 +29,8 @@ namespace TodoAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
